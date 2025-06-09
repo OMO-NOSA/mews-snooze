a594d6309683984028e0c190266a682b433a1358
Nosa Omorodion <24831235+OMO-NOSA@users.noreply.github.com>
2025-06-09 17:16:31 +0200

    chore(infra): corrected modules path

--- deploy-infra/prod/main.tf
+++ deploy-infra/prod/main.tf
@@ -1,5 +1,5 @@
 module "app" {
-  source = "../core-infra/"
+  source = "../../core-infra/"
   vpc_id                        = data.aws_vpc.default.id
   subnet_ids                    = tolist(data.aws_subnet_ids.default.ids)
   aws_region                    = "us-east-1"
